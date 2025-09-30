cfc7a4fe9e281f4b82cfb801a02d2f3b31fa8871
anamontero14 <ana.montero@iesnervion.es>
2025-09-30 13:45:36 +0200

    Cambios
    Botón

--- UNIDAD-3/Prueba/app/index.tsx
+++ UNIDAD-3/Prueba/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Button } from "@react-navigation/elements";
+import { Alert, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,7 +11,10 @@ export default function Index() {
       }}
     >
       <Text>Hola mundo</Text>
-      <Text>Hola miau</Text>
+      <Text>Hola</Text>
+      
+      <Button onPress={() => alert("Botón pulsado")}>Púlsame</Button>
+      
     </View>
   );
 }
