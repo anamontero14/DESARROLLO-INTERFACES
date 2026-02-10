4814db3447fdac213ba18c49adea7bbd40e6e1ba
anamontero14 <ana.montero@iesnervion.es>
2026-02-10 12:22:43 +0100

    Icono actualizado

--- UNIDAD-9/SignalRChat/SignalRChat/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ UNIDAD-9/SignalRChat/SignalRChat/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbeaebd174117f8693c6e7453be24caa2c26ef66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803bc986f768698da3ab46147e8ada5cc44273df")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
